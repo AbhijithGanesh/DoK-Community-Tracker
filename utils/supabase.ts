a1504b48808c61ddd0a2f6a03b3356b3db51671e
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-06-24 22:18:03 +0530

    Type Correction
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -4,4 +4,4 @@ const supabaseUrl: string | undefined = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey: string | undefined =
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
