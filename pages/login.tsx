7e2697ea560141efe62e1eb29fb35a641d79079d
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-06-26 16:13:14 +0530

    Addition of custom Auth
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,10 +2,8 @@ import { useState } from "react";
 import { supabase } from "../utils/supabase";
 import Layout from "./Layout";
 import { sign_in_wrapper } from "../utils/auth";
-import { FiGitlab } from "react-icons/fi";
-import { BiLockOpenAlt } from "react-icons/bi";
-import { FaGithubAlt, FaGoogle, FaMagic } from "react-icons/fa";
-import { NextRouter,  useRouter } from "next/router";
+import { NextRouter, useRouter } from "next/router";
+import { Auth as SupaAuth } from "@supabase/ui";
 
 let MagicLink = (): JSX.Element => {
   const [email, setEmail] = useState("");
@@ -37,82 +35,6 @@ let MagicLink = (): JSX.Element => {
   );
 };
 
-let GoogleAuth = (): JSX.Element => {
-  return (
-    <>
-      <section className="mx-2 p-2 flex bg-gray-700 text-white font-semibold rounded-md text-lg hover:-translate-y-2">
-        <button
-          onClick={async (e) => {
-            const { user, session, error } = await supabase.auth.signIn({
-              provider: "google",
-            });
-          }}
-        >
-          Google
-        </button>
-        <FaGoogle className="mx-2 my-auto text-2xl" />
-      </section>
-    </>
-  );
-};
-
-let GitLabAuth = (): JSX.Element => {
-  return (
-    <>
-      <section className="mx-2 py-2 px-2 flex justify-between w-min bg-gray-700 text-white font-semibold rounded-md text-lg hover:-translate-y-2">
-        <button
-          onClick={async (e) => {
-            const { user, session, error } = await supabase.auth.signIn({
-              provider: "gitlab",
-            });
-          }}
-        >
-          GitLab
-        </button>
-        <FiGitlab className="my-auto mx-2 text-2xl" />
-      </section>
-    </>
-  );
-};
-
-let GithubAuth = (): JSX.Element => {
-  return (
-    <>
-      <section className="mx-2 px-2 py-2 flex justify-between w-min bg-gray-700 text-white font-semibold rounded-md text-lg hover:-translate-y-2">
-        <button
-          onClick={async (e) => {
-            const { user, session, error } = await supabase.auth.signIn({
-              provider: "github",
-            });
-          }}
-        >
-          GitHub
-        </button>
-        <FaGithubAlt className="my-auto mx-2 text-2xl" />
-      </section>
-    </>
-  );
-};
-
-let SignUp = (): JSX.Element => {
-  let router: NextRouter = useRouter();
-  return (
-    <>
-      <section className="bg-gray-700 p-2 ml-2 mr-52 font-semibold rounded-md text-lg hover:-translate-y-2 flex justify-between">
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            router.push("/signup");
-          }}
-        >
-          Sign-up
-        </button>
-        <BiLockOpenAlt className="text-2xl m-2" />
-      </section>
-    </>
-  );
-};
-
 let Auth = (): JSX.Element | any => {
   const [magic, setMagic] = useState(false);
   const [password, setPassword] = useState("");
@@ -132,58 +54,16 @@ let Auth = (): JSX.Element | any => {
 
             {!magic ? (
               <>
-                <form>
-                  <input
-                    className="bg-gray-300 text-black font-regular text-2xl w-auto rounded-lg my-4 px-2 mx-4 justify center"
-                    type="email"
-                    name="email"
-                    placeholder="Your Email"
-                    autoComplete="username"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  <input
-                    className="bg-gray-300 text-black font-regular text-2xl w-auto rounded-lg my-4 px-2 mx-4 justify center"
-                    type="password"
-                    name="password"
-                    value={password}
-                    placeholder="Enter your password"
-                    onChange={(e) => setPassword(e.target.value)}
-                  ></input>
-                  <button
-                    className="bg-red-600 text-white font-bold text-xl rounded-lg m-2 py-1 px-2"
-                    onClick={async (e) => {
-                      e.preventDefault();
-                      setProcess(true);
-                      await sign_in_wrapper(email, password);
-                      if (supabase.auth.user()) {
-                        Router.push(`/profiles/${supabase.auth.user()?.id}`);
-                        setLoggedIn(true);
-                      }
-                      setProcess(false);
-                    }}
-                    disabled={process}
-                  >
-                    Login
-                  </button>
-                </form>
-                <section className="pl-4 py-4 flex flex-wrap gap-4 text-white font-semibold text-lg">
-                  <section className="text-white px-2 flex justify-between bg-gray-700 rounded-md font-regular text-lg hover:-translate-y-2">
-                    <button
-                      onClick={() => {
-                        setMagic(!magic);
-                      }}
-                    >
-                      Magic Link
-                    </button>
-                    <FaMagic className="mx-2 my-auto text-xl" />
-                  </section>
-
-                  <GoogleAuth />
-                  <GithubAuth />
-                  <GitLabAuth />
-                  <SignUp />
-                </section>
+                {" "}
+                <SupaAuth
+                  supabaseClient={supabase}
+                  providers={["google", "github", "gitlab"]}
+                  socialLayout="vertical"
+                  socialButtonSize="medium"
+                  magicLink={true}
+                  socialColors={false}
+                  className="font-extrabold hover:text-white"
+                />
               </>
             ) : (
               <>
